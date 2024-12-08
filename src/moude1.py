02542270224e167d767643e194a945e4fdb8e7df
Nahom Temesgen Nadew <126750827+chapi1420@users.noreply.github.com>
2024-12-08 05:56:54 +0000

    summary function added

--- src/moude1.py
+++ src/moude1.py
@@ -5,7 +5,7 @@ import seaborn as sns
 from scipy import stats
 import plotly.express as px
 import plotly.graph_objects as go
-from datetime import datetime
+import datetime
 
 #data is innitially loaded into pandas DataFrame
 def load(data_path):
@@ -13,5 +13,26 @@ def load(data_path):
     #timestamp data type changed
     df['Timestamp'] = pd.to_datetime(df['Timestamp'])
     return df
+
+def data_summary(data):
+    df = load(data)
+    numerics = df.select_dtypes(include=[np.number].columns)
+    return df[numerics].describe()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
